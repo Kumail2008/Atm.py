f2cf60ebc34741b3ca15115d2c7056e6b12e8bd4
Kumail2008 <69812462+Kumail2008@users.noreply.github.com>
2021-07-21 22:43:53 -0400

    Add files via upload

--- Atm.py
+++ Atm.py
@@ -1,17 +1,17 @@
 class Atm(object):
-    def __init__(user,pin,balance,):
+    def __init__(user,pin,balance):
         user.pin  = pin
         user.balance = balance
 
 
     def cashWithdrawal(user):
-        return(user.pin - withdraw)
+        return(user.balance - withdraw)
         
 kumail = Atm(1234,1000)
-password = int(input("Enter your pin kumail"))
-if password == user.pin :
+password = int(input("Enter your pin kumail: "))
+if password == kumail.pin :
     withdraw = int(input("how much do you want to withdraw"))
-    print("Your new balance is:")
+    print("Your new balance is:",end="")
     print(kumail.cashWithdrawal())
 else :
     print("incorrect password")
